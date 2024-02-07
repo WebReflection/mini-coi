8ce219208f661545bc87763af2fee00e9d44497f
webreflection <andrea.giammarchi@gmail.com>
2024-02-07 10:38:51 +0100

    Added -h | --help CLI flag

--- mini-cli.js
+++ mini-cli.js
@@ -6,12 +6,40 @@ const { writeFileSync, readFileSync, statSync } = require('fs');
 
 const { argv } = process;
 const args = argv.slice(2);
+const coi = ['--coi'];
 
 const cwd = process.cwd();
 
 for (let i = 0; i < args.length; i++) {
   if (args[i].startsWith('-')) {
     switch (args[i]) {
+      case '--cors':
+      case '--coop':
+      case '--coep':
+      case '--corp':
+        coi.pop();
+        break;
+      case '-h':
+      case '--help':
+        i = args.length;
+        console.log(`
+\x1b[7m\x1b[1m  mini-coi  \x1b[0m
+
+  \x1b[1moptions\x1b[0m
+
+    --service-worker | -sw    \x1b[2msave mini-coi.js script into a specific path:\x1b[0m
+                                \x1b[2mnpx mini-coi -sw ./public/\x1b[0m
+                                \x1b[2mbunx mini-coi -sw ./public/\x1b[0m
+
+  The \x1b[1m--coi\x1b[0m option is passed if none of these options are present:
+
+    --cors
+    --coop
+    --coep
+    --corp
+
+ ↓ all other options are forwarded to ↓`);
+        break;
       case '-sw':
       case '--service-worker':
         if (++i < args.length) {
@@ -31,7 +59,7 @@ for (let i = 0; i < args.length; i++) {
 
 spawn(
   resolve(__dirname, '..', 'static-handler', 'static-handler.cjs'),
-  ['--coi', ...args],
+  [...coi, ...args],
   {
     cwd,
     stdio: 'inherit',
